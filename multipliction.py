61062862f5f2402a56707701a34bbf61404bca0f
mscoder2012JetLearner <maxschwarz005@gmail.com>
2026-06-30 18:54:49 +0100

    Add files via upload

---
+++ multipliction.py
@@ -0,0 +1,25 @@
+from tkinter import*
+from tkinter.ttk import*
+
+
+window=Tk()
+window.title("multiplication")
+window.config(backgroun="white")
+window.geometry("400x400")
+title=Label(window,text="Multiplication Square")
+title.grid(row=0,column=2,padx=10,pady=25)
+sr=Label(window,text="Number and Range")
+sr.grid(row=2,column=0)
+store=IntVar()
+box=Combobox(window,textvariable=store,width=5)
+box.grid(row=2,column=1)
+box["values"]=tuple(range(0,101))
+rstore=IntVar()
+ten=Radiobutton(window,text="10",variable=rstore,value=10)
+twenty=Radiobutton(window,text="20",variable=rstore,value=20)
+thirty=Radiobutton(window,text="30",variable=rstore,value=30)
+ten.grid(row=3,column=3)
+twenty.grid(row=4,column=3)
+thirty.grid(row=5,column=3)
+rstore.set(10)
+window.mainloop()
