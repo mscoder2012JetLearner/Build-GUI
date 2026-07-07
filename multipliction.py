46b03a8ea28d2762b790237cad374bc05ad912a0
mscoder2012JetLearner <maxschwarz005@gmail.com>
2026-07-07 18:59:30 +0100

    Add files via upload

--- multipliction.py
+++ multipliction.py
@@ -1,5 +1,17 @@
 from tkinter import*
 from tkinter.ttk import*
+import tkinter as tk
+
+
+def multiplication_grid():
+    s=store.get()
+    r=rstore.get()
+    result=[]
+    for i in range(1,r):
+        a=s*i
+        result.append(s,"x",i,"=",a)
+    print(result)
+
 
 
 window=Tk()
@@ -8,7 +20,7 @@ window.config(backgroun="white")
 window.geometry("400x400")
 title=Label(window,text="Multiplication Square")
 title.grid(row=0,column=2,padx=10,pady=25)
-sr=Label(window,text="Number and Range")
+sr=Label(window,text="Number and Range",background="grey")
 sr.grid(row=2,column=0)
 store=IntVar()
 box=Combobox(window,textvariable=store,width=5)
@@ -21,7 +33,9 @@ thirty=Radiobutton(window,text="30",variable=rstore,value=30)
 ten.grid(row=3,column=3)
 twenty.grid(row=4,column=3)
 thirty.grid(row=5,column=3)
-show_grid=Button(window,text="show multiplication table",bd=4)
+show_grid=tk.Button(window,text="show multiplication table",bd=4,backgroun="green",command=multiplication_grid)
 show_grid.place(x=130,y=200)
+mgrid=Label(window)
+mgrid.place(x=0,y=250)
 rstore.set(10)
 window.mainloop()
